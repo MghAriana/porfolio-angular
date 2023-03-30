0392560f1f9cf5299b6137ea8f2ac8698e9a646b
MghAriana <arianaamarilla8@gmail.com>
2023-03-30 18:55:14 -0300

    c

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../service/productos.service';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
+
 
 @Component({
   selector: 'app-item',
@@ -6,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent {
+
+  producto: ProductoDescripcion | undefined;
+  id: string | undefined;
+
+
+  constructor(private route: ActivatedRoute ,
+              public productoService: ProductosService){}
+
+  ngOnInit() {
+    this.route.params
+      .subscribe( parametros =>{
+
+        console.log(parametros['id']);
+
+        this.productoService.getProducto(parametros['id'])
+              .subscribe( (producto: ProductoDescripcion) => {
+
+                this.producto = producto;
+                this.id = parametros['id']
+
+                console.log(producto);
+
+              })
+      })
+    
+  }
+
 
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { InfoPaginaService } from '../../service/info-pagina.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,17 @@ import { InfoPaginaService } from '../../service/info-pagina.service';
 })
 export class HeaderComponent {
 
-  constructor(public _servicio:InfoPaginaService){}
+  constructor(public _servicio:InfoPaginaService ,
+              private router:Router ){
+
+              }
+
+  buscarProducto( termino:string ){
+    if(termino.length < 1 ){
+      return;
+    }
+
+    this.router.navigate( ['/search' , termino])
+    console.log(termino)
+  }
 }
