6ef1806a961239ef1614f10d25a3b24b2bde1ff1
MghAriana <arianaamarilla8@gmail.com>
2023-03-30 18:54:06 -0300

    navegacion

--- src/app/service/productos.service.ts
+++ src/app/service/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { ProductoDescripcion } from '../interfaces/producto-descripcion.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,8 @@ export class ProductosService {
 
   productos: Producto[] = [];
 
+  productosFiltrado: Producto[] = [];
+
   constructor( private http: HttpClient) { 
 
     this.cargarProductos();
@@ -22,7 +25,8 @@ export class ProductosService {
 
   private cargarProductos() {
 
-    this.http.get <Producto[]> ('https://agular--html-default-rtdb.firebaseio.com/productos_idx.json')
+    return new Promise ( (resolve , rejet ) =>{
+         this.http.get <Producto[]> ('https://agular--html-default-rtdb.firebaseio.com/productos_idx.json')
     .subscribe( (resp: Producto[] ) => {
 
       console.log(resp);
@@ -33,8 +37,59 @@ export class ProductosService {
         this.cargando = false;
       },2000)
       
+      resolve();
 
     });
+    } )
+
+ 
 
 }
+  getProducto(id:string){
+
+    return this.http.get<ProductoDescripcion>(`https://agular--html-default-rtdb.firebaseio.com/productos/${ id }.json`);
+
+  }
+
+  buscarProducto(termino:string){
+
+    if( this.productos.length === 0 ){
+      //cargo producto
+      this.cargarProductos().then(()=> {
+        //cargar p
+        this.filtrarProductos( termino);
+
+      });
+    }else{
+      //aplicar filtro
+      this.filtrarProductos( termino);
+    }
+
+    this.productosFiltrado= this.productos.filter(producto =>{
+      return true;
+    });
+
+    console.log( this.productosFiltrado);
+  }
+
+  private filtrarProductos(termino:string){
+
+    console.log(this.productos);
+
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+
+
+    this.productos.forEach( prod =>{
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if (prod.categoria.indexOf( termino )>= 0  || prod.titulo.indexOf(termino) >=0 ){
+          this.productosFiltrado.push( prod );
+      }
+    } );
+  }
+
 }
